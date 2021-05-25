df967b3e6eee2c732b59833ebb76d7a3a1b7f419
Antoine07 <antoine.lucsko@gmail.com>
2021-05-25 14:47:32 +0200

    counter

--- 01_Introduction/Exercices/01_counter_letter.php
+++ 01_Introduction/Exercices/01_counter_letter.php
@@ -1,12 +1,16 @@
 <?php
 
 function numberLetter( string $letter, string $content){
+    $count  = 0;
     // $i = $i + 1  de manière équivalente $i++
     for($i=0; $i < strlen($content) ; $i = $i + 1 ){
-
+        // echo $content[$i] . "\n"; // . concaténation en PHP
+        if($letter == $content[$i]) {
+            $count++;
+        }
     }
-
     // pensez à retourner une valeur
+    return $count;
 }
 
-echo numberLetter('i','mississippi'); // 4
+echo numberLetter('i','mississippi') . "\n"; // 4
